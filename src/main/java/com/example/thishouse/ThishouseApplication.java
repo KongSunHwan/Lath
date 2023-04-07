02cf1dba33d5984c632ce73f482f44a388c91c70
KongSunHwan <tjsghks78@gmail.com>
2023-04-07 18:27:27 +0900

    스프링부트 기반 부동산 중개 웹사이트입니다.

---
+++ src/main/java/com/example/thishouse/ThishouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.thishouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThishouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThishouseApplication.class, args);
+	}
+
+}
