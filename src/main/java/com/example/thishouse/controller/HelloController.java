cc78cb2658a03d98001289485423a9fdb5f7ee3b
KongSunHwan <tjsghks78@gmail.com>
2023-04-27 16:22:44 +0900

    @feat: 디자인 UI 뷰 URL 매핑

--- src/main/java/com/example/thishouse/controller/HelloController.java
+++ src/main/java/com/example/thishouse/controller/HelloController.java
@@ -7,14 +7,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-        @RequestMapping("/hello")
-        public String hello(Model model) {
-            model.addAttribute("name", "Hello World!");
-            return "index";
-        }
 
-    @RequestMapping("/test")
-    public String test(Model model) {
-        return "test";
+    @RequestMapping("/map")
+    public String map() {
+            return "map";
+    }
+
+    @RequestMapping("/inquire")
+    public String inquire() {
+        return "inquire";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/main")
+    public String main() {
+        return "main";
+    }
+
+    @RequestMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @RequestMapping("/steamed_list")
+    public String steamed_list() {
+        return "steamed_list";
+    }
+
+    @RequestMapping("/real_estate_add")
+    public String real_estate_add() {
+        return "real_estate_add";
+    }
+
+    @RequestMapping("/lecture_intro")
+    public String lecture_intro() {
+        return "lecture_intro";
+    }
+
+    @RequestMapping("/header")
+    public String header() {
+        return "header";
+    }
+
+    @RequestMapping("/footer")
+    public String footer() {
+        return "footer";
     }
 }
