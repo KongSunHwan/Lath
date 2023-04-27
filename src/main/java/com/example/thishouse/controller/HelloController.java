84370c96e9e949add54178a4be155095c7ec39cf
KongSunHwan <tjsghks78@gmail.com>
2023-04-27 18:13:04 +0900

    @feat: 디자인 UI 뷰 URL 매핑

--- src/main/java/com/example/thishouse/controller/HelloController.java
+++ src/main/java/com/example/thishouse/controller/HelloController.java
@@ -10,71 +10,71 @@ public class HelloController {
 
     @RequestMapping("/map")
     public String map() {
-            return "map";
+            return "map/map";
     }
 
     @RequestMapping("/inquire")
     public String inquire() {
-        return "inquire";
+        return "inquire/inquire";
     }
 
     @RequestMapping("/login")
     public String login() {
-        return "login";
+        return "userlog/login";
     }
 
     @RequestMapping("/main")
     public String main() {
-        return "main";
+        return "main/main";
     }
 
     @RequestMapping("/signup")
     public String signup() {
-        return "signup";
+        return "userlog/signup";
     }
 
     @RequestMapping("/steamed_list")
     public String steamed_list() {
-        return "steamed_list";
+        return "steamed/steamed_list";
     }
 
     @RequestMapping("/real_estate_add")
     public String real_estate_add() {
-        return "real_estate_add";
+        return "real_estate/real_estate_add";
     }
 
-    @RequestMapping("/lecture_intro")
-    public String lecture_intro() {
-        return "lecture_intro";
+    @RequestMapping("/real_estate_intro")
+    public String real_estate_intro() {
+        return "real_estate_intro/real_estate_intro";
     }
 
     @RequestMapping("/header")
     public String header() {
-        return "header";
+        return "header/header";
     }
 
     @RequestMapping("/footer")
     public String footer() {
-        return "footer";
+        return "footer/footer";
     }
 
     @RequestMapping("/notice_add")
     public String notice_add() {
-        return "notice_add";
+        return "notice/notice_add";
     }
 
     @RequestMapping("/notice_detail")
     public String notice_detail() {
-        return "notice_detail";
+        return "notice/notice_detail";
     }
 
     @RequestMapping("/notice_edit")
     public String notice_edit() {
-        return "notice_edit";
+        return "notice/notice_edit";
     }
 
     @RequestMapping("/notice_list")
     public String notice_list() {
-        return "notice_list";
+        return "notice/notice_list";
     }
 }
