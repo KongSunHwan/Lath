6ae8615582325431ff72952f39f034b972f20cd5
auh99 <auh9904@gmail.com>
2023-05-08 14:53:45 +0900

    도메인 폴더에 테이블 DTO 작성

---
+++ src/main/java/com/example/thishouse/domain/house_detail.java
@@ -0,0 +1,16 @@
+package com.example.thishouse.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class house_detail {
+    private int house_num;
+    private String h_title;
+    private String h_content;
+}
--- src/main/java/com/example/thishouse/domain/tttt.java
+++
@@ -1,4 +0,0 @@
-package com.example.thishouse.domain;
-
-public class tttt {
-}
