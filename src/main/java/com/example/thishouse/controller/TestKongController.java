f6e8ac296221cd8d48f2d87b60613a2b0720cfb6
kim <ho9760@naver.com>
2023-08-09 16:00:42 +0900

    @feat:관리자 공지사항,커뮤니티 목록 페이징

--- src/main/java/com/example/thishouse/controller/TestKongController.java
+++ src/main/java/com/example/thishouse/controller/TestKongController.java
@@ -7,6 +7,7 @@ import com.example.thishouse.domain.community.Community_reply;
 import com.example.thishouse.service.AdminService;
 import com.example.thishouse.service.BoardService;
 import com.example.thishouse.service.MemberService;
+import com.example.thishouse.service.NoticeService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,8 +20,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestKongController {
     private final BoardService boardService;
-    private final MemberService memberService;
     private final AdminService adminService;
+    private final NoticeService noticeService;
 
 
 
@@ -251,7 +252,6 @@ public class TestKongController {
             System.out.println("검색내용 : " + context);
 
             List<Member> member_list_search = adminService.member_list_search(searchVO);
-            System.out.println("ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄹ");
             pagination.setTotalRecordCount(totCnt);
 
             searchVO.setEndDate(pagination.getLastPageNoOnPageList());
@@ -272,9 +272,144 @@ public class TestKongController {
         return "Admin_Dashboard/Statistics_Chart";
     }
 
-    @GetMapping("Board_Control")
-    public String Board_Control() {
-        return "Admin_Dashboard/Board_Control";
+    @GetMapping("Notice_Control")
+    public String Notice_Control(@ModelAttribute("searchVO") Notice searchVO, HttpServletRequest request, Model model) {
+        PageCtrl pagination  = new PageCtrl();
+        pagination.setCurrentPageNo(searchVO.getPageIndex());
+        pagination.setRecordCountPerPage(searchVO.getPageUnit());
+        pagination.setPageSize(searchVO.getPageSize());
+
+        searchVO.setFirstIndex(pagination.getFirstRecordIndex());
+        searchVO.setRecordCountPerPage(pagination.getRecordCountPerPage());
+        System.out.println("펄스트인덱스 : " + searchVO.getFirstIndex());
+
+        String search = request.getParameter("search");
+        String context = request.getParameter("searchValue");
+        System.out.println(search + " " + context);
+
+        if(context == null){
+            List<Notice> pg_list = noticeService.pg_list(searchVO);
+            model.addAttribute("pg_list" , pg_list);
+            int totCnt = noticeService.pg_listCnt();
+            model.addAttribute("totCnt",totCnt);
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("totalPageCnt",(int)Math.ceil(totCnt / (double)searchVO.getPageUnit()));
+            model.addAttribute("pagination",pagination);
+        }
+        else if(context != null && context == ""){
+            List<Notice> pg_list = noticeService.pg_list(searchVO);
+            model.addAttribute("pg_list" , pg_list);
+            int totCnt = noticeService.pg_listCnt();
+            model.addAttribute("totCnt",totCnt);
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("totalPageCnt",(int)Math.ceil(totCnt / (double)searchVO.getPageUnit()));
+            model.addAttribute("pagination",pagination);
+        }
+        else {
+            System.out.println("제대로 검색실행");
+            searchVO.setSearch_name(search);
+            searchVO.setSearch_content(context);
+            List<Notice> pg_list = noticeService.pg_list_search(searchVO);
+            int totCnt = noticeService.pg_list_searchcnt(searchVO);
+
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("pg_list", pg_list);
+            model.addAttribute("totCnt", totCnt);
+            model.addAttribute("totalPageCnt", (int) Math.ceil(totCnt / (double) searchVO.getPageUnit()));
+            model.addAttribute("pagination", pagination);
+        }
+        return "Admin_Dashboard/Notice_Control";
+    }
+
+    @GetMapping("Community_Control")
+    public String Community_Control(@ModelAttribute("searchVO") Community searchVO, HttpServletRequest request, Model model) {
+        PageCtrl pagination  = new PageCtrl();
+        pagination.setCurrentPageNo(searchVO.getPageIndex());
+        pagination.setRecordCountPerPage(searchVO.getPageUnit());
+        pagination.setPageSize(searchVO.getPageSize());
+
+        searchVO.setFirstIndex(pagination.getFirstRecordIndex());
+        searchVO.setRecordCountPerPage(pagination.getRecordCountPerPage());
+        System.out.println("펄스트인덱스 : " + searchVO.getFirstIndex());
+
+        String search = request.getParameter("searchName");
+        String context = request.getParameter("searchValue");
+        System.out.println(search + " " + context);
+
+        if(context == null){
+            List<Community> bd_list = boardService.bd_list(searchVO);
+            model.addAttribute("bd_list" , bd_list);
+            int totCnt = boardService.bd_listCnt();
+            model.addAttribute("totCnt",totCnt);
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("totalPageCnt",(int)Math.ceil(totCnt / (double)searchVO.getPageUnit()));
+            model.addAttribute("pagination",pagination);
+        }
+        else if(context != null && context == ""){
+            List<Community> bd_list = boardService.bd_list(searchVO);
+            model.addAttribute("bd_list" , bd_list);
+            int totCnt = boardService.bd_listCnt();
+            model.addAttribute("totCnt",totCnt);
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("totalPageCnt",(int)Math.ceil(totCnt / (double)searchVO.getPageUnit()));
+            model.addAttribute("pagination",pagination);
+        }
+        else{
+            System.out.println("제대로 검색실행");
+            searchVO.setSearch_name(search);
+            searchVO.setSearch_content(context);
+            List<Community> pg_list = boardService.bd_list_search(searchVO);
+            int totCnt = boardService.bd_list_search_Cnt(searchVO);
+
+            System.out.println("전체 게시글 수 : " + totCnt);
+
+            pagination.setTotalRecordCount(totCnt);
+
+            searchVO.setEndDate(pagination.getLastPageNoOnPageList());
+            searchVO.setStartDate(pagination.getFirstPageNoOnPageList());
+            searchVO.setPrev(pagination.getXprev());
+            searchVO.setNext(pagination.getXnext());
+            model.addAttribute("bd_list" , pg_list);
+            model.addAttribute("totCnt",totCnt);
+            model.addAttribute("totalPageCnt",(int)Math.ceil(totCnt / (double)searchVO.getPageUnit()));
+            model.addAttribute("pagination",pagination);
+        }
+        return "Admin_Dashboard/Community_Control";
     }
 
     @GetMapping("Report_List")
